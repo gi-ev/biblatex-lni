1cedf06e9ef13361f969f17b0eaa83fc60e44fe5
Oliver Kopp <kopp.dev@gmail.com>
2016-10-07 21:24:49 +0200

    Fix typo in release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#This script releases blibatex-lni in the format required by CTAN
+#This script releases biblatex-lni in the format required by CTAN
 
 #First create ZIP file containing versioned files only
-git archive -o blibatex-lni.zip --prefix=blibatex-lni/ HEAD
+git archive -o biblatex-lni.zip --prefix=biblatex-lni/ HEAD
 
 #Delete ".gitignore", ".gitattributes", and "release.sh". These files are not wanted by CTAN
 #In the future, other files starting with "." might be added, therefore we delete all files starting with "."
-zip blibatex-lni.zip -d /blibatex-lni/\.*
-zip blibatex-lni.zip -d ./blibatex-lni/release.sh
+zip biblatex-lni.zip -d /biblatex-lni/\.*
+zip biblatex-lni.zip -d ./biblatex-lni/release.sh
